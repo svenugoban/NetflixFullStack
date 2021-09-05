6ecda5068f21cdbfa85851b7872d0191d50e77b1
Your Name <you@example.com>
2021-09-05 19:41:30 +0530

    netflix completed

---
+++ admin/src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,52 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+            "Bearer "+JSON.parse(localStorage.getItem("user")).accessToken,
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUsers();
+  }, []);
+  
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {newUsers.map((user) => (
+          <li className="widgetSmListItem">
+            <img
+              src={
+                user.profilePic ||
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpccX-vEdN1MDEAoeaUI2chs4SBtZG1rFTGQ&usqp=CAU"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ admin/src/firebase.js
@@ -0,0 +1,18 @@
+// import firebase from "firebase";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA5oGUkDOFIALMyNcH848YUXy3pHq_ZKEo",
+//     authDomain: "netflixproject-a6096.firebaseapp.com",
+//     projectId: "netflixproject-a6096",
+//     storageBucket: "netflixproject-a6096.appspot.com",
+//     messagingSenderId: "716938949668",
+//     appId: "1:716938949668:web:147f270303b0dda22854b1",
+//     measurementId: "G-TXH6LK4CPE"
+//   };
+
+// firebase.initializeApp(firebaseConfig);
+// const storage = firebase.storage();
+// export default storage;
+
+
+
